ed0de064ef83ca176b9069e9d76877a80c1812db
samy <samykiiskinen@gmail.com>
2025-01-29 19:32:05 +0100

    Add form submission

--- app/worship/songs/new/page.tsx
+++ app/worship/songs/new/page.tsx
@@ -1,15 +1,40 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+
+interface SongForm {
+  title: string;
+  text: string;
+}
 
 const NewSongPage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<SongForm>();
   return (
-    <div className="max-w-md space-y-1">
-      <TextField.Root placeholder="Titel"></TextField.Root>
-      <SimpleMDE placeholder="Text" />
+    <form
+      className="max-w-md"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/songs", data);
+        router.push("/worship/songs");
+      })}
+    >
+      <TextField.Root
+        placeholder="Sångtitel"
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        name="text"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Sångtext" {...field} className="mt-2" />
+        )}
+      ></Controller>
       <Button>SPARA</Button>
-    </div>
+    </form>
   );
 };
 
