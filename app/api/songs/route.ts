6e2c0302f26e041859a7c99a2b2f14de39647203
samy <samykiiskinen@gmail.com>
2025-01-29 16:13:29 +0100

    Add API for creating songs

---
+++ app/api/songs/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod"
+import { prisma } from "@/prisma/client"
+
+const createSongSchema = z.object({
+    title: z.string().min(1).max(255),
+    text: z.string().min(1),
+})
+
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+    const validation = createSongSchema.safeParse(body)
+    if (!validation.success) 
+        return NextResponse.json(validation.error.errors, {status: 400})
+
+    const newSong = await prisma.song.create({
+         data: { title: body.title, text: body.text}
+    })
+
+    return NextResponse.json(newSong, { status: 201})
+}
