12feb9b30e385dac92350a8db2df549f0671f872
samy <samykiiskinen@gmail.com>
2025-02-03 19:34:57 +0100

    Hide navbar links if not authenticated

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -65,21 +65,22 @@ const NavBar = () => {
         </Flex>
       </Container>
       <ul className="flex space-x-4 border-b mb-5 px-5 h-20 items-center">
-        {links.map((link) => (
-          <li key={link.id}>
-            <Link
-              href={link.href}
-              className={classnames({
-                "": link.href === currentPath,
-                "": link.href !== currentPath,
-                "bg-slate-300 text-slate-700 font-bold border-2 border-black rounded-md px-2 py-1 text-2xl hover:bg-slate-200 hover:text-slate-700 hover:transition-colors hover:ease-in-out":
-                  true,
-              })}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
+        {status === "authenticated" &&
+          links.map((link) => (
+            <li key={link.id}>
+              <Link
+                href={link.href}
+                className={classnames({
+                  "": link.href === currentPath,
+                  "": link.href !== currentPath,
+                  "bg-slate-300 text-slate-700 font-bold border-2 border-black rounded-md px-2 py-1 text-2xl hover:bg-slate-200 hover:text-slate-700 hover:transition-colors hover:ease-in-out":
+                    true,
+                })}
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
       </ul>
     </nav>
   );
